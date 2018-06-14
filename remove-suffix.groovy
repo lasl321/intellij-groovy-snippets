fd53aa934d0dcbe83c3895e38f367279c6d7c56d
Luis Sotomayor <luis.sotomayor@stansonhealth.com>
2018-06-14 13:40:56 -0700

    Added helper

---
+++ remove-suffix.groovy
@@ -0,0 +1,9 @@
+static String removeSuffix(final String input, final String suffix) {
+    if (input.endsWith(suffix)) {
+        return input.substring(0, input.length() - suffix.length())
+    }
+
+    input
+}
+
+return removeSuffix(_1, _2)
